3e2e175408ffbf85ffb5537609774294675ff1a8
ChellaVigneshKP <chellavigneshkp123@gmail.com>
2025-11-16 09:35:36 +0530

    first commit

---
+++ src/main/java/com/chellavignesh/checkci/CheckCiApplication.java
@@ -0,0 +1,13 @@
+package com.chellavignesh.checkci;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheckCiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CheckCiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/chellavignesh/checkci/CheckCiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chellavignesh.checkci;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheckCiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
